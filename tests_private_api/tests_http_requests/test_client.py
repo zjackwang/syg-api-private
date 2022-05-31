4dd03ec92e36972f73575565517f0a065b071bb9
Ziqian Wang <ziqian.j.wang@gmail.com>
2022-05-31 12:04:41 -0500

    Timeouts to requests

--- tests_private_api/tests_http_requests/test_client.py
+++ tests_private_api/tests_http_requests/test_client.py
@@ -5,6 +5,7 @@ Fires off http requests to private api and tests return code
 
 from time import sleep
 import requests
+from requests.exceptions import Timeout
 import unittest
 import subprocess
 
@@ -25,17 +26,20 @@ def stop_local_api(api: subprocess.Popen):
 ##
 ## Secured requests w/ hmac sig
 ##
-def make_keyed_post_request(payload, url) -> requests.Response:
-    # print(payload)
-    # print(secret_key)
+def make_keyed_post_request(payload, url, timeout=5.0) -> requests.Response:
     hmac_sig = str(generate_hmac_signature(payload, secret_key))
-    # print(hmac_sig)
-    return requests.post(
-        url,
-        json=payload,
-        headers={"X-Syg-Api-Key": api_key, "X-HMAC-Signature": hmac_sig, "X-Is-Test-Request": 'True'},
-        timeout=3.0
-    )
+
+    try:
+        response = requests.post(
+            url,
+            json=payload,
+            headers={"X-Syg-Api-Key": api_key, "X-HMAC-Signature": hmac_sig, "X-Is-Test-Request": 'True'},
+            timeout=timeout
+        )
+    except Timeout:
+        raise Timeout
+
+    return response
 
 ##
 ## Endpoints
@@ -46,6 +50,9 @@ USER_SUBMITTED_GENERIC_ITEM_LOCAL = "http://localhost:5000/usersubmittedgenerici
 USER_SUBMITTED_MATCHED_ITEM_LOCAL = "http://localhost:5000/usersubmittedmatcheditemset"
 USER_UPDATED_GENERIC_ITEM_LOCAL = "http://localhost:5000/userupdatedgenericitemset"
 ## Remote endpoints
+USER_SUBMITTED_GENERIC_ITEM_REMOTE = "https://syg-user-submitted.herokuapp.com/usersubmittedgenericitemset"
+USER_SUBMITTED_MATCHED_ITEM_REMOTE = "https://syg-user-submitted.herokuapp.com/usersubmittedmatcheditemset"
+USER_UPDATED_GENERIC_ITEM_REMOTE = "https://syg-user-submitted.herokuapp.com/userupdatedgenericitemset"
 
 ### TODO: NEGATIVE TESTS 
 class PrivateLocalAPITests(unittest.TestCase):
@@ -66,7 +73,10 @@ class PrivateLocalAPITests(unittest.TestCase):
             'Links': ''
         }
 
-        response = make_keyed_post_request(payload, USER_SUBMITTED_GENERIC_ITEM_LOCAL)
+        try: 
+            response = make_keyed_post_request(payload, USER_SUBMITTED_GENERIC_ITEM_LOCAL)
+        except Timeout: 
+            self.fail("Request timed out")
 
         failure_msg = f"Request failed. Response: {response.content}"
         self.assertEqual(
@@ -80,8 +90,11 @@ class PrivateLocalAPITests(unittest.TestCase):
             'ScannedItemName': 'Not Random',
             'GenericItemName': 'Random',
         }
-
-        response = make_keyed_post_request(payload, USER_SUBMITTED_MATCHED_ITEM_LOCAL)
+        
+        try: 
+            response = make_keyed_post_request(payload, USER_SUBMITTED_MATCHED_ITEM_LOCAL)
+        except Timeout: 
+            self.fail("Request timed out")
 
         failure_msg = f"Request failed. Response: {response.content}"
         self.assertEqual(
@@ -116,7 +129,10 @@ class PrivateLocalAPITests(unittest.TestCase):
             }
         }
 
-        response = make_keyed_post_request(payload, USER_UPDATED_GENERIC_ITEM_LOCAL)
+        try:
+            response = make_keyed_post_request(payload, USER_UPDATED_GENERIC_ITEM_LOCAL)
+        except Timeout: 
+            self.fail("Request timed out")
 
         failure_msg = f"Request failed. Response: {response.content}"
         self.assertEqual(
@@ -130,6 +146,89 @@ class PrivateLocalAPITests(unittest.TestCase):
         stop_local_api(self.api)
         print(self.api.poll())
 
+
+class PrivateRemoteAPITests(unittest.TestCase):
+    def test_user_submitted_generic_item_post(self):
+        payload = {
+            'Name': 'Random',
+            'Category': 'Produce',
+            'Subcategory': 'Fresh',
+            'IsCut': False, 
+            'DaysInFridge': 30.0,
+            'DaysOnShelf': 30.0,
+            'DaysInFreezer': 240.0,
+            'Notes': '',
+            'Links': ''
+        }
+
+        try:
+            response = make_keyed_post_request(payload, USER_SUBMITTED_GENERIC_ITEM_REMOTE)
+        except Timeout: 
+            self.fail("Request timed out")
+
+        failure_msg = f"Request failed. Response: {response.content}"
+        self.assertEqual(
+            response.status_code,
+            200,
+            msg=failure_msg,
+        )
+
+    def test_user_submitted_matched_item_post(self):
+        payload = {
+            'ScannedItemName': 'Not Random',
+            'GenericItemName': 'Random',
+        }
+
+        try:
+            response = make_keyed_post_request(payload, USER_SUBMITTED_MATCHED_ITEM_REMOTE)
+        except Timeout: 
+            self.fail("Request timed out")
+
+        failure_msg = f"Request failed. Response: {response.content}"
+        self.assertEqual(
+            response.status_code,
+            200,
+            msg=failure_msg,
+        )
+
+    def test_user_updated_generic_item_post(self):
+        payload = {
+            'Original': {
+                'Name': 'Random',
+                'Category': 'Produce',
+                'Subcategory': 'Fresh',
+                'IsCut': False, 
+                'DaysInFridge': 30.0,
+                'DaysOnShelf': 30.0,
+                'DaysInFreezer': 240.0,
+                'Notes': '',
+                'Links': ''
+            },
+            'Updated': {
+                'Name': 'Random',
+                'Category': 'Produce',
+                'Subcategory': 'Fresh',
+                'IsCut': False, 
+                'DaysInFridge': 30.0,
+                'DaysOnShelf': 35.0,
+                'DaysInFreezer': 240.0,
+                'Notes': '',
+                'Links': ''
+            }
+        }
+        
+        try:
+            response = make_keyed_post_request(payload, USER_UPDATED_GENERIC_ITEM_REMOTE, timeout=7.5)
+        except Timeout: 
+            self.fail("Request timed out")
+
+        failure_msg = f"Request failed. Response: {response.content}"
+        self.assertEqual(
+            response.status_code,
+            200,
+            msg=failure_msg,
+        )
+
 def test_suite_local_api():
     suite = unittest.TestSuite()
     suite.addTest(PrivateLocalAPITests("test_user_submitted_generic_item_post"))
@@ -140,6 +239,9 @@ def test_suite_local_api():
 
 def test_suite_remote_api():
     suite = unittest.TestSuite()
+    suite.addTest(PrivateRemoteAPITests("test_user_submitted_generic_item_post"))
+    suite.addTest(PrivateRemoteAPITests("test_user_submitted_matched_item_post"))
+    suite.addTest(PrivateRemoteAPITests("test_user_updated_generic_item_post"))
     return suite
 
 
